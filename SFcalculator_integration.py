3a25305ba2dc01869726b7532a1ee6fd24804fb3
Vidya Ganapati <vganapati@gmail.com>
2024-06-25 12:50:12 -0700

    expand SFcalculator_integration

--- SFcalculator_integration.py
+++ SFcalculator_integration.py
@@ -1,7 +1,12 @@
 from SFC_Torch import SFcalculator
+import reciprocalspaceship as rs
+import torch
 
-pdb_file = '../SFcalculator_torch/tests/data/1dur.pdb'
-mtz_file = '../SFcalculator_torch/tests/data/1dur.mtz'
+# pdb_file = '../SFcalculator_torch/tests/data/1dur.pdb'
+# mtz_file = '../SFcalculator_torch/tests/data/1dur.mtz'
+
+pdb_file = '../SFcalculator_torch/tests/data/6ry3.pdb'
+mtz_file = '../SFcalculator_torch/tests/data/6ry3_phases.mtz'
 
 sfcalculator = SFcalculator(pdb_file, mtz_file, expcolumns=['FP', 'SIGFP'], set_experiment=True, freeflag='FREE', testset_value=0)
 
@@ -21,9 +26,21 @@ sfcalculator.calc_fsolvent()
 # stored in sfcalculator.kmasks, sfcalculator.uanisos, sfcalculator.kisos
 # If you want do a further optimization, there is 
 # sfcalculator.get_scales_adam() or sfcalculator.get_scales_lbfgs()
+# determines a set of scale values for each resolution bin
 sfcalculator.init_scales(requires_grad=True)
 
 # Get the Fmodel for future loss function construction
 Fmodel = sfcalculator.calc_ftotal()
-
+Fmodel_amplitude = torch.abs(Fmodel)
 sfcalculator.summarize()
+
+sfcalculator.HKL_array
+rs.read_mtz(mtz_file).FP # equivalent to sfcalculator.Fo
+rs.read_mtz(mtz_file).SIGFP # equivalent to sfcalculator.SigF
+rs.read_mtz(mtz_file).FC # deposited calculated value
+rs.read_mtz(mtz_file).get_hkls()
+rs.read_mtz(mtz_file)["FC"][0,0,4]
+
+# check why # of HKLs is different
+
+breakpoint()
